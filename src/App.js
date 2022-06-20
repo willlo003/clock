05cfd30b59e52c665b51c407d9b06fc9ce86f683
willlo003 <willlo@hotmail.com.hk>
2022-06-20 18:58:14 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+  const [localDate, setLocalDate] = useState();
+  const [isActive, setIsActive] = useState(true);
+  const [isActiveLocal, setIsActiveLocal] = useState(true);
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    fetchDate()
+  }, [])
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setDate(new Date());
+      }, 1000)
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive])
+
+  useEffect(() => {
+    let interval = null;
+    if (isActiveLocal) {
+      interval = setInterval(() => {
+        setLocalDate(new Date().toLocaleTimeString('en-US', {
+          timeZone: location,
+        }))
+      }, 1000)
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActiveLocal])
+
+  const toggle = () => {
+    setIsActive(!isActive);
+  }
+  const toggleLocal = () => {
+    setIsActiveLocal(!isActiveLocal);
+  }
+
+  const fetchDate = async () => {
+    let data = await axios.get('http://worldtimeapi.org/api/timezone/Asia/Hong_Kong')
+    if (data.status === 200) {
+      let localTime = new Date()
+      setLocalDate( localTime.toLocaleTimeString('en-US', {
+        timeZone: data.data.timezone,
+      }))
+      setLocation(data.data.timezone)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {date.toLocaleTimeString()}
+      </h1>
+      <button onClick={() => toggle()}>{isActive ? 'stop' : 'start'}</button>
+      <h1>
+        {localDate}
+      </h1>
+      <button onClick={() => toggleLocal()}>{isActiveLocal ? 'stop' : 'start'}</button>
     </div>
   );
 }
